84857c2ac2c52963d1669a9e26d5b01e41143fa9
name <varunbrahmane30@gmail.com>
2021-06-08 06:47:50 +0530

    Added : UC7-EmpWageFunction

--- empwage.sh
+++ empwage.sh
@@ -3,7 +3,7 @@
 # CONSTATNTS FOR THE PROGRAM
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
-MAX_HRS_IN_MONTH=100;
+MAX_HRS_IN_MONTH=10;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
@@ -11,21 +11,27 @@ NUM_WORKING_DAYS=20;
 totalEmpHrs=0;
 totalWorkingDays=0;
 
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
-do
-((totalWorkingDays++))
-empCheck=$(($RANDOM%3));
+
+function getWorkHrs(){
+local empCheck=$1
 case $empCheck in
   $IS_FULL_TIME)
       empHrs=8
          ;;
   $IS_PART_TIME)
        empHrs=4
-        ;;
- *)
+   ;;
+  *)
     empHrs=0
         ;;
 esac
+echo $empHrs
+}
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+((totalWorkingDays++))
+empCheck=$(($RANDOM%3));
+empHrs=$( getWorkHrs $empCheck )
 totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
 done
 
