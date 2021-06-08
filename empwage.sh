e1585ea34dcec03cb0e15d2c1cc1acd45b99d9b3
name <varunbrahmane30@gmail.com>
2021-06-08 06:30:55 +0530

    Added : UC5-EmpMonthlyWage

--- empwage.sh
+++ empwage.sh
@@ -1,27 +1,32 @@
 #!/bin/bash -x
 
+# CONSTATNTS FOR THE PROGRAM
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
-isPartTime=1;
-isFullTime=2;
-totalSalary=0;
-empRatePerHr=20;
-numWorkingDays=2;
+# VARIABLES
+totalEmpHrs=0;
+totalWorkingDays=0;
 
-for (( day=1; day<=$numWorkingDays; day++ ))
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-  empCheck=$(($RANDOM%3));
-   case $empCheck in
-     $isFullTime)
-           empHrs=8
-      ;;
-   $isPartTime)
-      empHrs=4
-      ;;
-   *)
-      empHrs=0
-      ;;
-      esac
-   salary=$(($empHrs * $empRatePerHr));
-      totalSalary=$(($totalSalary+$salary));
+((totalWorkingDays++))
+empCheck=$(($RANDOM%3));
+case $empCheck in
+  $IS_FULL_TIME)
+      empHrs=8
+         ;;
+  $IS_PART_TIME)
+       empHrs=4
+        ;;
+ *)
+    empHrs=0
+        ;;
+esac
+totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
 done
 
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
